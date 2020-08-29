58342b0d56458d6f3eadca41705b26a7d2892ac9
patilaarti15 <65292226+patilaarti15@users.noreply.github.com>
2020-08-29 23:02:01 +0530

    Add files via upload

---
+++ day6/modfun2/my_mod/mod2.js
@@ -0,0 +1,14 @@
+const num =3.1415;
+const bool =false;
+const str ="NodeJs";
+
+const arr=[1,2,3,4];
+const arr_str=["aarti","dipak","rohan"];
+const myjson={id:1,name:"aarti"};
+const list=[{id:2,title:"A"},{id:3,title:"B"},{id:4,title:"C"}];
+
+module.exports{
+    num,bool,str,
+    arr,arr_str,
+    myjson,list,
+}
