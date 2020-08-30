c4fafd2f0a25ed3f3f7cc3b47c2d77c0a4986615
patilaarti15 <65292226+patilaarti15@users.noreply.github.com>
2020-08-31 00:01:47 +0530

    Add files via upload

--- summarized_demo/main.js
+++ summarized_demo/main.js
@@ -1,1 +1,10 @@
-console.log("Hello World!!!");
+let mymod1 = require("./my.mod1");
+let mymod2 = require("./my.mod2");
+const PI=mymod1.pi;
+console.log("Hello CDAC...!!!",PI);
+
+const res1=mymod2.sum(5, 5);
+const res2=mymod2.diff(5, 1);
+console.log(res1);
+console.log(res2);
+
---
+++ summarized_demo/my.mod1.js
@@ -0,0 +1,4 @@
+let pi=3.141509;
+let str="Hello CDAC-juhu";
+let str1="Welcome to CDAC Mumbai";
+module.exports={pi,str,str1};
