bc6726f80ca17a6fb2a155776532c7780d324506
patilaarti15 <65292226+patilaarti15@users.noreply.github.com>
2020-08-31 00:00:08 +0530

    Add files via upload

---
+++ summarized_demo/1.js
@@ -0,0 +1,16 @@
+console.log("hello CDAC!!!!");
+console.log("hello CDAC-JUHU");
+let a=100;
+const b=200;
+var c=300;
+
+let str="Aarti";
+let pi=3.14159;
+let correct=true;
+
+let json={id:1 ,title:"aarti"}
+let arr=[1,true,'aarti']
+
+console.log(a,b,c);
+console.log(pi,correct);
+console.log(json,arr);
---
+++ summarized_demo/2.js
@@ -0,0 +1,12 @@
+//approch-1
+function sample(){console.log("sample Output...1");};
+
+//approch-2
+let sample1=function(){console.log("sample Output...2");};
+
+//approch-3
+var sample2=()=>{console.log("sample Output...3");};
+
+sample();
+sample1();
+sample2();
